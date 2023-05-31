7410d419c943109455ebb6ed4878c9549a966328
KelvinOyanna <dotkelplus@gmail.com>
2023-05-31 18:25:29 +0100

    update the logic to write generated log data into an external JSON file

--- util.py
+++ util.py
@@ -33,44 +33,43 @@ import json
 import random
 
 
-def generate_log_data(num_lines):
-    for i in range(num_lines):
-        event_date = datetime.now().strftime("%Y%m%d")
-        event_timestamp = f"{int(datetime.now().timestamp() * 1000000)}"
-        event_name = random.choice(["push_received", "notification_clicked", "app_opened"])
-        event_params = []
-        for j in range(random.randint(1, 5)):
-            key = "".join(random.choices(string.ascii_lowercase, k=10))
-            value_type = random.choice(["string_value", "int_value"])
-            value = {"string_value": "".join(random.choices(string.ascii_lowercase, k=10)), "int_value": random.randint(1, 1000)}
-            event_params.append({"key": key, "value": {value_type: value[value_type]}})
-        event_server_timestamp_offset = f"{random.randint(1, 1000000)}"
-        user_pseudo_id = "".join(random.choices(string.ascii_lowercase, k=32))
-        user_properties = []
-        for k in range(random.randint(1, 10)):
-            key = "".join(random.choices(string.ascii_lowercase, k=10))
-            value_type = random.choice(["string_value", "int_value"])
-            value = {"string_value": "".join(random.choices(string.ascii_lowercase, k=10)), "int_value": random.randint(1, 1000)}
-            timestamp_micros = f"{int(datetime.now().timestamp() * 1000000)}"
-            user_properties.append({"key": key, "value": {value_type: value[value_type], "set_timestamp_micros": timestamp_micros}})
-        user_first_touch_timestamp = f"{int(datetime.now().timestamp() * 1000)}"
-        mobile_os_hardware_model = "".join(random.choices(string.ascii_uppercase + string.digits, k=3))
-        language = "en-us"
-        time_zone_offset_seconds = "-18000"
-        version = "11.3.1"
-        log_dict = {"event_date": event_date, "event_timestamp": event_timestamp, "event_name": event_name,
-                    "event_params": event_params, "event_server_timestamp_offset": event_server_timestamp_offset,
-                    "user_pseudo_id": user_pseudo_id, "user_properties": user_properties,
-                    "user_first_touch_timestamp": user_first_touch_timestamp,
-                    "mobile_os_hardware_model": mobile_os_hardware_model, "language": language,
-                    "time_zone_offset_seconds": time_zone_offset_seconds, "version": version}
-        yield log_dict
-
-def generate_log_file(num_lines, filename):
-    with open(filename, "w") as json_file:
-        for log_dict in generate_log_data(num_lines):
+def generate_log_file(file_name, num_lines):
+    def generate_logs(num_lines):
+        for i in range(num_lines):
+            event_date = datetime.now().strftime("%Y%m%d")
+            event_timestamp = f"{int(datetime.now().timestamp() * 1000000)}"
+            event_name = random.choice(["push_received", "notification_clicked", "app_opened"])
+            event_params = []
+            for j in range(random.randint(1, 5)):
+                key = "".join(random.choices(string.ascii_lowercase, k=10))
+                value_type = random.choice(["string_value", "int_value"])
+                value = {"string_value": "".join(random.choices(string.ascii_lowercase, k=10)), "int_value": random.randint(1, 1000)}
+                event_params.append({"key": key, "value": {value_type: value[value_type]}})
+            event_server_timestamp_offset = f"{random.randint(1, 1000000)}"
+            user_pseudo_id = "".join(random.choices(string.ascii_lowercase, k=32))
+            user_properties = []
+            for k in range(random.randint(1, 10)):
+                key = "".join(random.choices(string.ascii_lowercase, k=10))
+                value_type = random.choice(["string_value", "int_value"])
+                value = {"string_value": "".join(random.choices(string.ascii_lowercase, k=10)), "int_value": random.randint(1, 1000)}
+                timestamp_micros = f"{int(datetime.now().timestamp() * 1000000)}"
+                user_properties.append({"key": key, "value": {value_type: value[value_type], "set_timestamp_micros": timestamp_micros}})
+            user_first_touch_timestamp = f"{int(datetime.now().timestamp() * 1000)}"
+            mobile_os_hardware_model = "".join(random.choices(string.ascii_uppercase + string.digits, k=3))
+            language = "en-us"
+            time_zone_offset_seconds = "-18000"
+            version = "11.3.1"
+            log_dict = {"event_date": event_date, "event_timestamp": event_timestamp, "event_name": event_name,
+                        "event_params": event_params, "event_server_timestamp_offset": event_server_timestamp_offset,
+                        "user_pseudo_id": user_pseudo_id, "user_properties": user_properties,
+                        "user_first_touch_timestamp": user_first_touch_timestamp,
+                        "mobile_os_hardware_model": mobile_os_hardware_model, "language": language,
+                        "time_zone_offset_seconds": time_zone_offset_seconds, "version": version}
+            yield log_dict
+    with open(file_name, "w") as json_file:
+        for log_dict in generate_logs(num_lines):
             json.dump(log_dict, json_file, indent=4)
     print('log files written to external JSON')
 
 
-generate_log_file(100, 'log_gen_file.json')
+generate_log_file('log_gen_file.json', 100)
